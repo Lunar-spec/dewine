11816c099cb8c68a9f21b08c431def863e6999be
Manjesh Singh <smanjesh1854@gmail.com>
2024-05-01 09:33:57 +0530

    added: basic structure for product

--- app/(protected)/_components/ProductForm.tsx
+++ app/(protected)/_components/ProductForm.tsx
@@ -3,6 +3,10 @@ const ProductForm = () => {
     <div>
       ProductForm for both creation and editing by using a prop called
       type=update or type=create
+      Take out the Event Form and utilize it
+
+      1. First work on creating it
+      2. Creating product
     </div>
   );
 };
